acbdd3d40942a2912b7b45c4b079c1d0fe71b598
SugarDark <joey861121@gmail.com>
2020-08-02 22:14:50 +0800

    moveplane

--- Assets/C#/Character.cs
+++ Assets/C#/Character.cs
@@ -53,145 +53,12 @@ public abstract class Character : MonoBehaviour
         //Debug.Log(stones.Length);
         //劃出可行走位置
         int i;
+
+
+
         int j;
-        for(i=0;i< planes.Length; i++)
-        {
-            int count = 0;
-            if(Mathf.Round(Math.Abs(planes[i].transform.position.x - pos.x)) + Mathf.Round(Math.Abs(planes[i].transform.position.z - pos.z)) <= dis)
-            {
-                for (j = 0; j < stones.Length; j++)
-                {
-                    if ((Mathf.Round(stones[j].transform.position.x) == Mathf.Round(planes[i].transform.position.x)) && (Mathf.Round(stones[j].transform.position.z) == Mathf.Round(planes[i].transform.position.z)))
-                    {
-                        break;
-                    }
-                    else
-                    {
-                        count++;
-                    }
-                    if(count== stones.Length)
-                    {
-                        planes[i].GetComponent<MeshRenderer>().material.color = Color.blue;
-                        planes[i].GetComponent<CanMovePlane>().Obj1 = this;
-                    }
-                }
-            }
-        }
-        
 
 
-        for (i = 0; i < GameObject.Find("CharacterOrder").GetComponent<CharacterOrder>().characters.Count; i++) //設置角色所在地面
-        {
-            GameObject.Find("CharacterOrder").GetComponent<CharacterOrder>().characters[i].planeSet();
-        }
-    }
-
-    public virtual void attackDisplay(float attackDis)
-    {
-        clearDisplay();
-        GameObject[] planes;
-        planes = GameObject.FindGameObjectsWithTag("MovePlane");
-        int i;
-        for (i = 0; i < planes.Length; i++)
-        {
-            if (Mathf.Round(Math.Abs(planes[i].transform.position.x - pos.x)) + Mathf.Round(Math.Abs(planes[i].transform.position.z - pos.z)) <= attackDis)
-            {
-
-                planes[i].GetComponent<MeshRenderer>().material.color = Color.red;
-                planes[i].GetComponent<CanMovePlane>().Obj1 = this;
-            }
-        }
-
-        for (i=0;i< GameObject.Find("CharacterOrder").GetComponent<CharacterOrder>().characters.Count; i++)
-        {
-            GameObject.Find("CharacterOrder").GetComponent<CharacterOrder>().characters[i].planeSet();
-        }
-        
-    }
-
-    public virtual void clearDisplay()
-    {
-        GameObject[] planes;
-        planes = GameObject.FindGameObjectsWithTag("MovePlane");
-        int i;
-        for (i = 0; i < planes.Length; i++)
-        {
-            planes[i].GetComponent<MeshRenderer>().material.color = Color.clear;
-        }
-
-        for (i = 0; i < GameObject.Find("CharacterOrder").GetComponent<CharacterOrder>().characters.Count; i++)
-        {
-            GameObject.Find("CharacterOrder").GetComponent<CharacterOrder>().characters[i].planeSet();
-        }
-    }
-
-    public virtual void attack(Character Obj1)
-    {
-        Obj1.hp = Obj1.hp - (STR - Obj1.DEF);
-        Debug.Log(Obj1.name+"已受到"+ (STR - Obj1.DEF) +"點攻擊");
-
-        canvasController.Instance.enemyName.GetComponent<Text>().text = Obj1.characterName;
-        canvasController.Instance.enemyHp.GetComponent<Text>().text = Obj1.hp + "/" + Obj1.hpMax;
-        canvasController.Instance.enemySp.GetComponent<Text>().text = Obj1.sp + "/" + Obj1.spMax;
-
-        damageFloatUp.GetComponent<DamageFloatUp>().beAttack(Obj1, (STR - Obj1.DEF));
-        clearDisplay();                 //攻擊完清除藍色地板
-        GameObject.Find("Canvas").GetComponent<canvasController>().attack.GetComponent<Button>().interactable = false;
-
-        if (Obj1.hp <= 0)
-        {
-            GameObject.Find("CharacterOrder").GetComponent<CharacterOrder>().characters.Remove(Obj1);
-            Obj1.gameObject.SetActive(false);
-        }
-
-        GameObject.Find("CharacterOrder").GetComponent<CharacterOrder>().checkEnd();
-    }
-
-    public virtual void move(float x, float z)
-    {
-        
-    }
-
-    public virtual void menuShow()
-    {
-        canvasController.Instance.menuShow();
-    }
-
-    public virtual void automove()
-    {
-
-
-
-
-
-    }
-
-    private void OnMouseEnter()
-    {
-        if (plane.GetComponent<MeshRenderer>().material.color == Color.red &&
-            GameObject.Find("CharacterOrder").GetComponent<CharacterOrder>().characters[0] != this)
-        {
-            canvasController.Instance.enemyName.GetComponent<Text>().text = characterName;
-            canvasController.Instance.enemyHp.GetComponent<Text>().text = hp + "/" + hpMax;
-            canvasController.Instance.enemySp.GetComponent<Text>().text = sp + "/" + spMax;
-            canvasController.Instance.enemyImage.GetComponent<Image>().sprite = Resources.Load<Sprite>(image);
-            canvasController.Instance.enemyName.SetActive(true);
-            canvasController.Instance.enemyHp.SetActive(true);
-            canvasController.Instance.enemySp.SetActive(true);
-            canvasController.Instance.enemyImage.enabled = true;
-        }
-    }
-
-    private void OnMouseExit()
-    {
-        canvasController.Instance.enemyName.SetActive(false);
-        canvasController.Instance.enemyHp.SetActive(false);
-        canvasController.Instance.enemySp.SetActive(false);
-        canvasController.Instance.enemyImage.enabled = false;
-    }
-
-    private void movebutt()
-    {
         List<float[]> enemy = new List<float[]>();          //敵人
         List<float[]> partner = new List<float[]>();        //隊友
         List<float> mCount = new List<float>();             //canMove走到哪一個後剩下幾步可以走 (A能走幾步)
@@ -218,14 +85,6 @@ public abstract class Character : MonoBehaviour
             entry[1] = Mathf.Round(element.transform.position.z);
             obs.Add(entry);
         }
-
-
-
-
-
-
-
-
 
 
         foreach (Character element in GameObject.Find("CharacterOrder").GetComponent<CharacterOrder>().characters)           //分隊伍
@@ -495,6 +354,26 @@ public abstract class Character : MonoBehaviour
         }
 
 
+ //       foreach(float[] x in canMove)
+//        {
+ //           Debug.Log(x[0] + "," + x[1]);
+ //       }
+
+        Debug.Log(planes[0].transform.position.x + "," + planes[0].transform.position.z);
+        
+
+
+        for (i = 0; i < planes.Length; i++)
+        {
+            for (j = 0; j < canMove.Count; j++)
+            {
+                if (Mathf.Round(planes[i].transform.position.x)==canMove[j][0]&& Mathf.Round(planes[i].transform.position.z)==canMove[j][1])
+                {
+                    planes[i].GetComponent<MeshRenderer>().material.color = Color.blue;
+                    planes[i].GetComponent<CanMovePlane>().Obj1 = this;
+                }
+            }
+        }
 
 
 
@@ -502,9 +381,118 @@ public abstract class Character : MonoBehaviour
 
 
 
+        
+        
+
+
+        for (i = 0; i < GameObject.Find("CharacterOrder").GetComponent<CharacterOrder>().characters.Count; i++) //設置角色所在地面
+        {
+            GameObject.Find("CharacterOrder").GetComponent<CharacterOrder>().characters[i].planeSet();
+        }
+    }
+
+    public virtual void attackDisplay(float attackDis)
+    {
+        clearDisplay();
+        GameObject[] planes;
+        planes = GameObject.FindGameObjectsWithTag("MovePlane");
+        int i;
+        for (i = 0; i < planes.Length; i++)
+        {
+            if (Mathf.Round(Math.Abs(planes[i].transform.position.x - pos.x)) + Mathf.Round(Math.Abs(planes[i].transform.position.z - pos.z)) <= attackDis)
+            {
+
+                planes[i].GetComponent<MeshRenderer>().material.color = Color.red;
+                planes[i].GetComponent<CanMovePlane>().Obj1 = this;
+            }
+        }
+
+        for (i=0;i< GameObject.Find("CharacterOrder").GetComponent<CharacterOrder>().characters.Count; i++)
+        {
+            GameObject.Find("CharacterOrder").GetComponent<CharacterOrder>().characters[i].planeSet();
+        }
+        
+    }
+
+    public virtual void clearDisplay()
+    {
+        GameObject[] planes;
+        planes = GameObject.FindGameObjectsWithTag("MovePlane");
+        int i;
+        for (i = 0; i < planes.Length; i++)
+        {
+            planes[i].GetComponent<MeshRenderer>().material.color = Color.clear;
+        }
+
+        for (i = 0; i < GameObject.Find("CharacterOrder").GetComponent<CharacterOrder>().characters.Count; i++)
+        {
+            GameObject.Find("CharacterOrder").GetComponent<CharacterOrder>().characters[i].planeSet();
+        }
+    }
+
+    public virtual void attack(Character Obj1)
+    {
+        Obj1.hp = Obj1.hp - (STR - Obj1.DEF);
+        Debug.Log(Obj1.name+"已受到"+ (STR - Obj1.DEF) +"點攻擊");
+
+        canvasController.Instance.enemyName.GetComponent<Text>().text = Obj1.characterName;
+        canvasController.Instance.enemyHp.GetComponent<Text>().text = Obj1.hp + "/" + Obj1.hpMax;
+        canvasController.Instance.enemySp.GetComponent<Text>().text = Obj1.sp + "/" + Obj1.spMax;
+
+        damageFloatUp.GetComponent<DamageFloatUp>().beAttack(Obj1, (STR - Obj1.DEF));
+        clearDisplay();                 //攻擊完清除藍色地板
+        GameObject.Find("Canvas").GetComponent<canvasController>().attack.GetComponent<Button>().interactable = false;
+
+        if (Obj1.hp <= 0)
+        {
+            GameObject.Find("CharacterOrder").GetComponent<CharacterOrder>().characters.Remove(Obj1);
+            Obj1.gameObject.SetActive(false);
+        }
+
+        GameObject.Find("CharacterOrder").GetComponent<CharacterOrder>().checkEnd();
+    }
+
+    public virtual void move(float x, float z)
+    {
+        
+    }
+
+    public virtual void menuShow()
+    {
+        canvasController.Instance.menuShow();
+    }
+
+    public virtual void automove()
+    {
 
 
 
+
+
+    }
+
+    private void OnMouseEnter()
+    {
+        if (plane.GetComponent<MeshRenderer>().material.color == Color.red &&
+            GameObject.Find("CharacterOrder").GetComponent<CharacterOrder>().characters[0] != this)
+        {
+            canvasController.Instance.enemyName.GetComponent<Text>().text = characterName;
+            canvasController.Instance.enemyHp.GetComponent<Text>().text = hp + "/" + hpMax;
+            canvasController.Instance.enemySp.GetComponent<Text>().text = sp + "/" + spMax;
+            canvasController.Instance.enemyImage.GetComponent<Image>().sprite = Resources.Load<Sprite>(image);
+            canvasController.Instance.enemyName.SetActive(true);
+            canvasController.Instance.enemyHp.SetActive(true);
+            canvasController.Instance.enemySp.SetActive(true);
+            canvasController.Instance.enemyImage.enabled = true;
+        }
+    }
+
+    private void OnMouseExit()
+    {
+        canvasController.Instance.enemyName.SetActive(false);
+        canvasController.Instance.enemyHp.SetActive(false);
+        canvasController.Instance.enemySp.SetActive(false);
+        canvasController.Instance.enemyImage.enabled = false;
     }
 
 
